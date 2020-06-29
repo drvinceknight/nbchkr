9ce401ca1b3bad5fd05d2862c3ee342274eb1050
Vince Knight <vincent.knight@gmail.com>
2020-06-29 16:12:05 +0100

    Write function to read nb to a dictionary.

--- tests/test_release.py
+++ tests/test_release.py
@@ -10,9 +10,38 @@ import nbchkr.utils
 
 import pathlib
 
-NB_PATH = pathlib.Path("./nbs")
+def get_absolute_path_of_test_directory():
+    return pathlib.Path(__file__).parent.absolute()
 
-def test_read_nb():
+
+NB_PATH = get_absolute_path_of_test_directory() / "nbs/"
+
+
+def test_read_nb_gives_dictionary():
     nb_path = NB_PATH / "test.ipynb"
     nb = nbchkr.utils.read(nb_path=nb_path)
-    assert type(nb) is str
+    assert type(nb) is dict
+
+
+def test_read_nb_gives_expected_keys():
+    nb_path = NB_PATH / "test.ipynb"
+    nb = nbchkr.utils.read(nb_path=nb_path)
+    expected_keys = {
+            'cells',
+            'metadata',
+            'nbformat',
+            'nbformat_minor',
+            }
+    assert set(nb.keys()) == expected_keys
+
+
+def test_read_nb_cells_gives_list():
+    """
+    The `expected_length` variable corresponds to the number of cells in
+    `tests/nbs/test.ipynb`. As new cells are added this should be updated.
+    """
+    nb_path = NB_PATH / "test.ipynb"
+    nb = nbchkr.utils.read(nb_path=nb_path)
+    expected_length = 2
+    assert type(nb["cells"]) is list
+    assert len(nb["cells"]) == expected_length

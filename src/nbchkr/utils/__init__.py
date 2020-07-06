7ac9e38498d7cc9118027495d939f849dae3270c
Vince Knight <vincent.knight@gmail.com>
2020-07-06 17:52:47 +0100

    Write checker functionality (#9)
    * Add coverage report to gitignore.
    
    * Add code to add checks to submitted notebook.
    
    * Read notebook using nbformat.
    
    * Add utility functions.
    
    Functions to:
    
    1. Read all tags and return a string.
    2. Read tags and find score from given regex.
    
    * Add func to check answers.
    
    * Tidy various aspects:
    
    - Run black
    - Fix pyflake failures
    - Add missing docstrings
    
    * Add requirements.
    
    * Missing tags to notebook.
    
    * Remove windows support.

--- src/nbchkr/utils/__init__.py
+++ src/nbchkr/utils/__init__.py
@@ -2,20 +2,27 @@ import json
 import re
 import pathlib
 
+from typing import Tuple, Optional
+
+import nbformat  # type: ignore
+from nbconvert.preprocessors import ExecutePreprocessor  # type: ignore
+
 TAGS_REGEX_PATTERNS_TO_IGNORE = ["hide", r"score:\d"]
 SOLUTION_REGEX = re.compile(
     r"### BEGIN SOLUTION[\s\S](.*?)[\s\S]### END SOLUTION", re.DOTALL
 )
+ANSWER_TAG_REGEX = r"answer:*"
+SCORE_REGEX = re.compile(r"score:(\d+)")
 
 
-def read(nb_path: pathlib.Path) -> dict:
+def read(nb_path: pathlib.Path, as_version: int = 4) -> dict:
     """
     Read a jupyter notebook file at `nb_path`.
 
     Returns the python `dict` representation.
     """
-    contents = nb_path.read_text()
-    nb = json.loads(contents)
+    with open(nb_path, "r") as f:
+        nb = nbformat.read(f, as_version=as_version)
     return nb
 
 
@@ -63,3 +70,101 @@ def write(output_path: pathlib.Path, nb_json):
     Write the python dict representation of a notebook to `output_path`.
     """
     output_path.write_text(json.dumps(nb_json))
+
+
+def add_checks(nb_json: dict, source_nb_json: dict, answer_tag_regex=None) -> dict:
+    """
+    Given a `nb_json` and a source `source_nb_json`, add the cells in
+    `source_nb` with tags matching `answer_tag_regex` to `source_nb_json`
+
+    This is used to add a student's answers to the source notebook.
+    """
+    if answer_tag_regex is None:
+        answer_tag_regex = ANSWER_TAG_REGEX
+    answers = {
+        tag: cell
+        for cell in nb_json["cells"]
+        for tag in cell["metadata"].get("tags", [])
+        if bool(re.match(pattern=answer_tag_regex, string=tag))
+    }
+    for i, cell in enumerate(source_nb_json["cells"]):
+        for tag in cell["metadata"].get("tags", []):
+            if tag in answers:
+                source_nb_json["cells"][i] = answers[tag]
+    return source_nb_json
+
+
+def get_tags(cell: dict, tag_seperator: str = "|") -> Optional[str]:
+    """
+    Given a `cell` of a notebook, return a string with all tags separated by
+    `|`.
+    """
+    try:
+        return tag_seperator.join(cell["metadata"]["tags"])
+    except KeyError:
+        return None
+
+
+def get_score(cell: dict, score_regex_pattern=None) -> int:
+    """
+    Given a `cell` of a notebook, return the score as defined by the
+    `score_regex_pattern`.
+    """
+    if score_regex_pattern is None:
+        score_regex_pattern = SCORE_REGEX
+    tags = get_tags(cell)
+    if tags is not None:
+        search = re.search(pattern=score_regex_pattern, string=tags)
+        try:
+            return int(search.group(1))  # type: ignore
+        except AttributeError:
+            return 0
+    return 0
+
+
+def check(
+    nb_node: dict, timeout: int = 600, score_regex_pattern=None
+) -> Tuple[int, int, str]:
+    """
+    Given a `nb_node`, it executes the notebook and keep track of the score.
+
+    This returns 3 things:
+
+    - The student score
+    - The total score obtainable
+    - Some feedback in markdown format
+    """
+    if score_regex_pattern is None:
+        score_regex_pattern = SCORE_REGEX
+
+    ep = ExecutePreprocessor(timeout=timeout, allow_errors=True)
+    ep.preprocess(nb_node)
+
+    total_score = 0
+    maximum_score = 0
+    feedback_md = ""
+
+    for cell in nb_node["cells"]:
+        if get_score(cell) > 0:
+            score = get_score(cell)
+            maximum_score += score
+            try:
+                outputs = cell["outputs"][0]
+                if outputs["output_type"] == "error":
+                    question_feedback = outputs["evalue"]
+                    feedback_md += f"""
+{question_feedback}
+
+0 / {score}
+"""
+            except IndexError:
+                assertion = cell["source"]
+                feedback_md += f"""
+Assertion passed:
+
+    {assertion}
+
+{score} / {score}
+"""
+                total_score += score
+    return total_score, maximum_score, feedback_md
---
+++ tests/test_check.py
@@ -0,0 +1,89 @@
+"""
+Tests for the check functionality
+
+    - Replace relevant cells with read in solutions.
+    - Check scoring cells run.
+    - Write feedback.
+
+"""
+import nbchkr.utils
+
+import nbformat
+
+from test_release import NB_PATH
+
+
+def test_read_nb_gives_dictionary():
+    nb_path = NB_PATH / "submission.ipynb"
+    nb = nbchkr.utils.read(nb_path=nb_path)
+    assert type(nb) is nbformat.NotebookNode
+
+
+def test_add_checks_creates_notebook_with_assertions():
+    nb_json = nbchkr.utils.read(nb_path=NB_PATH / "submission.ipynb")
+    source_nb_json = nbchkr.utils.read(nb_path=NB_PATH / "test.ipynb")
+    nb_with_checks = nbchkr.utils.add_checks(
+        nb_json=nb_json, source_nb_json=source_nb_json
+    )
+    assert "assert _ == 55" in str(nb_with_checks)
+    assert "sum(i for i in range(10))" in str(nb_with_checks)
+
+    output_path = NB_PATH / "feedback.ipynb"
+    nbchkr.utils.write(output_path=output_path, nb_json=nb_with_checks)
+
+
+def test_check_with_no_errors_for_original_source():
+    nb_node = nbchkr.utils.read(nb_path=NB_PATH / "test.ipynb")
+    score, maximum_score, feedback = nbchkr.utils.check(nb_node=nb_node)
+    expected_score = 10
+    assert score == expected_score
+    assert maximum_score == expected_score
+
+    expected_feedback = """
+Assertion passed:
+
+    assert _ == 55, "That is not the correct answer"
+
+3 / 3
+
+Assertion passed:
+
+    assert sum_of_digits.__doc__ is not None, "You did not include a docstring"
+
+2 / 2
+
+Assertion passed:
+
+    for n in range(1, 10):
+    assert sum_of_digits(n=n) == n * (n + 1) / 2, f"You function did not give the correct score for n={n}"
+
+5 / 5
+"""
+    assert feedback == expected_feedback
+
+
+def test_check_with_no_errors_for_test_submission():
+    nb_node = nbchkr.utils.read(nb_path=NB_PATH / "submission.ipynb")
+    source_nb_node = nbchkr.utils.read(nb_path=NB_PATH / "test.ipynb")
+    nb_node = nbchkr.utils.add_checks(nb_json=nb_node, source_nb_json=source_nb_node)
+    score, maximum_score, feedback = nbchkr.utils.check(nb_node=nb_node)
+    expected_score = 2
+    expected_maximum_score = 10
+    assert score == expected_score
+    assert maximum_score == expected_maximum_score
+    expected_feedback = """
+That is not the correct answer
+
+0 / 3
+
+Assertion passed:
+
+    assert sum_of_digits.__doc__ is not None, "You did not include a docstring"
+
+2 / 2
+
+You function did not give the correct score for n=1
+
+0 / 5
+"""
+    assert feedback == expected_feedback

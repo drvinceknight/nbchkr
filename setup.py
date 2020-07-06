3c3751cd418b2ca008c6d73a3ee3044b50b837bf
Vince Knight <vincent.knight@gmail.com>
2020-07-06 17:22:04 +0100

    Add requirements.

--- setup.py
+++ setup.py
@@ -3,12 +3,19 @@ from setuptools import find_packages, setup
 # Read in the version number
 # exec(open("src/nashpy/version.py", "r").read())
 
+# Read in the requirements.txt file
+with open("requirements.txt") as f:
+    requirements = []
+    for library in f.read().splitlines():
+        requirements.append(library)
+
 setup(
     name="nbchkr",
     author="Vince Knight",
     author_email=("knightva@cardiff.ac.uk"),
     packages=find_packages("src"),
     package_dir={"": "src"},
+    install_requires=requirements,
     url="",
     license="The MIT License (MIT)",
     description="A lightweight tool to grade notebook assignements",

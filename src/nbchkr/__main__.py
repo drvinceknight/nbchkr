f2b73fa26f94ba2626ea47f691b47ace3eb9f91d
Vince Knight <vincent.knight@gmail.com>
2020-07-08 17:12:24 +0100

    Write command to release notebook.

--- src/nbchkr/__main__.py
+++ src/nbchkr/__main__.py
@@ -1,6 +1,20 @@
+import pathlib
 import click
 
+import nbchkr.utils
 
-@click.command()
+@click.group()
 def main():
-    print("hello")
+    pass
+
+@main.command()
+@click.option('--source', help="The path to the source ipynb file")
+@click.option('--output', help="The path to the destination ipynb file")
+def release(source, output):
+    nb_path = pathlib.Path(source)
+    nb_node = nbchkr.utils.read(nb_path=nb_path)
+    nbchkr.utils.remove_cells(nb_node=nb_node)
+
+    output_path = pathlib.Path(output)
+    nbchkr.utils.write(output_path=output_path, nb_node=nb_node)
+    click.echo(f"Solutions removed from {source}. New notebook written to {output}.")
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,11 +2,38 @@
 Tests for the command line tool.
 """
 import subprocess
+import pathlib
+import nbchkr
+
+from test_release import NB_PATH
 
 def test_help_call():
     output = subprocess.run(["nbchkr", "--help"], capture_output=True)
-    expected_stdout = b'Usage: nbchkr [OPTIONS]\n\nOptions:\n  --help  Show this message and exit.\n'
+    expected_stdout = b"""Usage: nbchkr [OPTIONS] COMMAND [ARGS]...
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  release
+"""
     assert output.stdout == expected_stdout
     assert output.stderr == b''
 
-def test
+def test_release():
+    # TODO Add better tear down.
+    output = subprocess.run(["nbchkr", "release", "--source", f"{NB_PATH}/test.ipynb", "--output", "student.ipynb"], capture_output=True)
+    expected_stdout = str.encode(f'Solutions removed from {NB_PATH}/test.ipynb. New notebook written to student.ipynb.\n')
+    assert output.stderr == b''
+    assert output.stdout == expected_stdout
+
+    student_nb = nbchkr.utils.read(nb_path="student.ipynb")
+    expected_length = 4
+    assert type(student_nb["cells"]) is list
+    assert len(student_nb["cells"]) == expected_length
+    # TODO Add a better pytest cleanup.
+    try:
+        pathlib.Path("student.ipynb").unlink()
+    except FileNotFoundError:  # TODO Ensure py3.8 is used so that can pass
+        # `missing_ok=True` to `path.unlink`.
+        pass

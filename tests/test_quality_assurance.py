4174734994d90b420f93391b47bcb47a1c8372b2
Vince Knight <vincent.knight@gmail.com>
2020-07-08 10:56:06 +0100

    Write a function to check if tags match.
    This closes #10

---
+++ tests/test_quality_assurance.py
@@ -0,0 +1,27 @@
+""""
+Tests to check quality assurance of notebooks.
+"""
+import nbchkr.utils
+
+from test_release import NB_PATH
+
+def test_check_tags_when_they_match():
+    nb_node = nbchkr.utils.read(nb_path=NB_PATH / "submission.ipynb")
+    source_nb_node = nbchkr.utils.read(nb_path=NB_PATH / "test.ipynb")
+    nb_node = nbchkr.utils.add_checks(nb_node=nb_node, source_nb_node=source_nb_node)
+    assert nbchkr.utils.check_tags_match(source_nb_node=source_nb_node, nb_node=nb_node) is True
+
+
+def test_check_tags_when_they_do_not_match():
+    """
+    The tags should not match here as they the check tags have not been added.
+    """
+    nb_node = nbchkr.utils.read(nb_path=NB_PATH / "submission.ipynb")
+    source_nb_node = nbchkr.utils.read(nb_path=NB_PATH / "test.ipynb")
+    assert nbchkr.utils.check_tags_match(source_nb_node=source_nb_node, nb_node=nb_node) is False
+
+
+def test_check_tags_when_they_do_not_match_for_specific_case():
+    nb_node = nbchkr.utils.read(nb_path=NB_PATH / "submission_with_missing_tags.ipynb")
+    source_nb_node = nbchkr.utils.read(nb_path=NB_PATH / "test.ipynb")
+    assert nbchkr.utils.check_tags_match(source_nb_node=source_nb_node, nb_node=nb_node) is False
